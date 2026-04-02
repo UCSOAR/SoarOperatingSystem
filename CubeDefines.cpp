9cbfb9760ecfb3dc849fa88e9f1fe7185ca88a14
Spiro Douvis <spirodouvis@gmail.com>
2026-04-01 20:09:40 -0600

    Added new soar_log macro, builds without errors, untested

--- CubeDefines.cpp
+++ CubeDefines.cpp
@@ -4,8 +4,8 @@
  *  Created on: Oct 18, 2023
  *      Author: Chris
  */
-#include <cstdarg>        // Support for va_start and va_end
-#include <cstring>        // Support for strlen and strcpy
+#include <cstdarg> // Support for va_start and va_end
+#include <cstring> // Support for strlen and strcpy
 
 #include "Core/Inc/Mutex.hpp"
 #include "Core/Inc/Command.hpp"
@@ -13,46 +13,106 @@
 #include "CubeDefines.hpp"
 #include "SystemDefines.hpp"
 #include "CubeTask.hpp"
+#include "LoggingService.hpp"
 
 /* Global Variables ------------------------------------------------------------------*/
 Mutex Global::vaListMutex;
 
 /* System Functions ------------------------------------------------------------*/
 /**
-* @brief Variadic print function, sends a command packet to the queue
-* @param str String to print with printf style formatting
-* @param ... Additional arguments to print if assertion fails, in same format as printf
-*/
-void cube_print(const char* str, ...)
+ * @brief Variadic print function, sends a command packet to the queue
+ * @param str String to print with printf style formatting
+ * @param ... Additional arguments to print if assertion fails, in same format as printf
+ */
+void cube_print(const char *str, ...)
 {
 #ifndef DISABLE_DEBUG
-    //Try to take the VA list mutex
-    if (Global::vaListMutex.Lock(DEBUG_TAKE_MAX_TIME_MS)) {
+    // Try to take the VA list mutex
+    if (Global::vaListMutex.Lock(DEBUG_TAKE_MAX_TIME_MS))
+    {
         // If we have a message, and can use VA list, extract the string into a new buffer, and null terminate it
         uint8_t str_buffer[DEBUG_PRINT_MAX_SIZE] = {};
         va_list argument_list;
         va_start(argument_list, str);
-        int16_t buflen = vsnprintf(reinterpret_cast<char*>(str_buffer), sizeof(str_buffer) - 1, str, argument_list);
+        int16_t buflen = vsnprintf(reinterpret_cast<char *>(str_buffer), sizeof(str_buffer) - 1, str, argument_list);
         va_end(argument_list);
-        if (buflen > 0) {
+        if (buflen > 0)
+        {
             str_buffer[buflen] = '\0';
         }
 
         // Release the VA List Mutex
         Global::vaListMutex.Unlock();
 
-        //Generate a command
+        // Generate a command
         Command cmd(DATA_COMMAND, (uint16_t)CUBE_TASK_COMMAND_SEND_DEBUG); // Set the UART channel to send data on
 
-        //Copy data into the command
+        // Copy data into the command
         cmd.CopyDataToCommand(str_buffer, buflen);
 
-        //Send this packet off to the UART Task
+        // Send this packet off to the UART Task
         CubeTask::Inst().GetEventQueue()->Send(cmd, false);
     }
     else
     {
         // Print out that we could not acquire the VA list mutex
+        SOAR_ASSERT(false, "Could not acquire VA_LIST mutex");
+    }
+#endif
+}
+
+/**
+ * @brief Variadic log function, prints to UART and logs to external flash
+ * @param str String to log with printf style formatting
+ * @param ... Additional arguments to log, in same format as printf
+ */
+void cube_log(const char *str, ...)
+{
+#ifndef DISABLE_DEBUG
+    // Try to take the VA list mutex
+    if (Global::vaListMutex.Lock(DEBUG_TAKE_MAX_TIME_MS))
+    {
+        uint8_t str_buffer[DEBUG_PRINT_MAX_SIZE] = {};
+        va_list argument_list;
+        va_start(argument_list, str);
+        int16_t buflen = vsnprintf(reinterpret_cast<char *>(str_buffer), sizeof(str_buffer) - 1, str, argument_list);
+        va_end(argument_list);
+
+        if (buflen > 0)
+        {
+            str_buffer[buflen] = '\0';
+        }
+        else
+        {
+            buflen = 0;
+        }
+
+        // Release the VA List Mutex
+        Global::vaListMutex.Unlock();
+
+        // ===== PART 1: Send to UART (same as SOAR_PRINT) =====
+        Command cmd(DATA_COMMAND, (uint16_t)CUBE_TASK_COMMAND_SEND_DEBUG);
+        cmd.CopyDataToCommand(str_buffer, buflen);
+        CubeTask::Inst().GetEventQueue()->Send(cmd, false);
+
+        // ===== PART 2: Log to external flash with timestamp =====
+        // Prepend timestamp for context
+        uint32_t timestamp = xTaskGetTickCount() * portTICK_PERIOD_MS;
+        uint8_t timestamped_buf[DEBUG_PRINT_MAX_SIZE];
+        int32_t ts_len = snprintf(reinterpret_cast<char *>(timestamped_buf),
+                                  sizeof(timestamped_buf) - 1,
+                                  "[%lu] %s",
+                                  timestamp,
+                                  (const char *)str_buffer);
+
+        if (ts_len > 0)
+        {
+            // Log to flash via LoggingService (length prefix is added automatically)
+            LoggingService::LogText(timestamped_buf, static_cast<uint32_t>(ts_len));
+        }
+    }
+    else
+    {
         SOAR_ASSERT(false, "Could not acquire VA_LIST mutex");
     }
 #endif
@@ -66,9 +126,11 @@ void cube_print(const char* str, ...)
  * @param str Optional message to print if assertion fails. Must be less than 192 characters AFTER formatting
  * @param ... Additional arguments to print if assertion fails, in same format as printf
  */
-void cube_assert_debug(bool condition, const char* file, const uint16_t line, const char* str, ...) {
+void cube_assert_debug(bool condition, const char *file, const uint16_t line, const char *str, ...)
+{
     // If assertion succeeds, do nothing
-    if (condition) {
+    if (condition)
+    {
         return;
     }
 
@@ -80,41 +142,47 @@ void cube_assert_debug(bool condition, const char* file, const uint16_t line, co
     // NOTE: https://nadler.com/embedded/newlibAndFreeRTOS.html
 
     // We have an assert fail, we try to take control of the Debug semaphore, and then suspend all other parts of the system
-    if (Global::vaListMutex.Lock(ASSERT_TAKE_MAX_TIME_MS)) {
+    if (Global::vaListMutex.Lock(ASSERT_TAKE_MAX_TIME_MS))
+    {
         // We have the mutex, we can now safely print the message
         printMessage = true;
     }
 
     vTaskSuspendAll();
 
-    //If we have the vaListMutex, we can safely use vsnprintf
-    if (printMessage) {
+    // If we have the vaListMutex, we can safely use vsnprintf
+    if (printMessage)
+    {
         // Print out the assertion header through the supported interface, we don't have a UART task running, so we directly use HAL
         uint8_t header_buf[ASSERT_BUFFER_MAX_SIZE] = {};
-        int16_t res = snprintf(reinterpret_cast<char*>(header_buf), ASSERT_BUFFER_MAX_SIZE - 1, "\r\n\n-- ASSERTION FAILED --\r\nFile [%s] @ Line # [%d]\r\n", file, line);
-        if (res < 0) {
+        int16_t res = snprintf(reinterpret_cast<char *>(header_buf), ASSERT_BUFFER_MAX_SIZE - 1, "\r\n\n-- ASSERTION FAILED --\r\nFile [%s] @ Line # [%d]\r\n", file, line);
+        if (res < 0)
+        {
             // If we failed to generate the header, just format the line number
-            snprintf(reinterpret_cast<char*>(header_buf), ASSERT_BUFFER_MAX_SIZE - 1, "\r\n\n-- ASSERTION FAILED --\r\nFile [PATH_TOO_LONG] @ Line # [%d]\r\n", line);
+            snprintf(reinterpret_cast<char *>(header_buf), ASSERT_BUFFER_MAX_SIZE - 1, "\r\n\n-- ASSERTION FAILED --\r\nFile [PATH_TOO_LONG] @ Line # [%d]\r\n", line);
         }
 
         // Output the header to the debug port
-        DEFAULT_DEBUG_UART_DRIVER->Transmit(header_buf, strlen(reinterpret_cast<char*>(header_buf)));
+        DEFAULT_DEBUG_UART_DRIVER->Transmit(header_buf, strlen(reinterpret_cast<char *>(header_buf)));
 
         // If we have a message, and can use VA list, extract the string into a new buffer, and null terminate it
-        if (printMessage && str != nullptr) {
+        if (printMessage && str != nullptr)
+        {
             uint8_t str_buffer[ASSERT_BUFFER_MAX_SIZE] = {};
             va_list argument_list;
             va_start(argument_list, str);
-            int16_t buflen = vsnprintf(reinterpret_cast<char*>(str_buffer), sizeof(str_buffer) - 1, str, argument_list);
+            int16_t buflen = vsnprintf(reinterpret_cast<char *>(str_buffer), sizeof(str_buffer) - 1, str, argument_list);
             va_end(argument_list);
-            if (buflen > 0) {
+            if (buflen > 0)
+            {
                 str_buffer[buflen] = '\0';
                 DEFAULT_DEBUG_UART_DRIVER->Transmit(str_buffer, buflen);
             }
         }
     }
-    else {
-        DEFAULT_DEBUG_UART_DRIVER->Transmit((uint8_t*)"-- ASSERTION FAILED --\r\nCould not acquire vaListMutex\r\n", 55);
+    else
+    {
+        DEFAULT_DEBUG_UART_DRIVER->Transmit((uint8_t *)"-- ASSERTION FAILED --\r\nCould not acquire vaListMutex\r\n", 55);
     }
 
 #endif
--- CubeDefines.hpp
+++ CubeDefines.hpp
@@ -1,26 +1,27 @@
 /**
-  ******************************************************************************
-  * @file           : CubeDefines.hpp
-  * @brief          : Cube++ Macros and wrappers
-  ******************************************************************************
-  *
-  * Contains system wide macros, defines, and wrappers
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : CubeDefines.hpp
+ * @brief          : Cube++ Macros and wrappers
+ ******************************************************************************
+ *
+ * Contains system wide macros, defines, and wrappers
+ *
+ ******************************************************************************
+ */
 #ifndef CUBE_PLUSPLUS_CUBE_DEFINES_HPP_
 #define CUBE_PLUSPLUS_CUBE_DEFINES_HPP_
 
 /* System Wide Includes ------------------------------------------------------------------*/
-#include "CubeUtils.hpp"  // Utility functions
-#include <cstdint>        // For uint32_t, etc.
+#include "CubeUtils.hpp" // Utility functions
+#include <cstdint>       // For uint32_t, etc.
 #include <cstdio>        // Standard c printf, vsnprintf, etc.
 #include "cmsis_os.h"    // CMSIS RTOS definitions
 #include "Mutex.hpp"
 
 /* Global Functions ------------------------------------------------------------------*/
-void cube_print(const char* format, ...);
-void cube_assert_debug(bool condition, const char* file, uint16_t line, const char* str = nullptr, ...);
+void cube_print(const char *format, ...);
+void cube_log(const char *format, ...);
+void cube_assert_debug(bool condition, const char *file, uint16_t line, const char *str = nullptr, ...);
 
 /* Global Variable Interfaces ------------------------------------------------------------------*/
 /* All must be extern from CubeDefines.cpp -------------------------------------------------*/
@@ -33,18 +34,18 @@ namespace Global
 /* - Each define / constexpr must have a comment explaining what it is used for     */
 /* - Each define / constexpr must be all-caps. Prefer constexpr unless it's a string, or a calculation (eg. mathematical expression being more readable) */
 // RTOS
-constexpr uint8_t DEFAULT_QUEUE_SIZE = 10;                    // Default size of the queue
-constexpr uint16_t MAX_NUMBER_OF_COMMAND_ALLOCATIONS = 100;    // Let's assume ~128B per allocation, 100 x 128B = 12800B = 12.8KB
+constexpr uint8_t DEFAULT_QUEUE_SIZE = 10;                  // Default size of the queue
+constexpr uint16_t MAX_NUMBER_OF_COMMAND_ALLOCATIONS = 100; // Let's assume ~128B per allocation, 100 x 128B = 12800B = 12.8KB
 
 // DEBUG
-constexpr uint16_t DEBUG_TAKE_MAX_TIME_MS = 500;        // Max time in ms to take the debug semaphore
-constexpr uint16_t DEBUG_SEND_MAX_TIME_MS = 500;        // Max time the assert fail is allowed to wait to send header and message to HAL
-constexpr uint16_t DEBUG_PRINT_MAX_SIZE = 192;            // Max size in bytes of message print buffers
+constexpr uint16_t DEBUG_TAKE_MAX_TIME_MS = 500; // Max time in ms to take the debug semaphore
+constexpr uint16_t DEBUG_SEND_MAX_TIME_MS = 500; // Max time the assert fail is allowed to wait to send header and message to HAL
+constexpr uint16_t DEBUG_PRINT_MAX_SIZE = 192;   // Max size in bytes of message print buffers
 
 // ASSERT
-constexpr uint16_t ASSERT_BUFFER_MAX_SIZE = 160;        // Max size in bytes of assert buffers (assume x2 as we have two message segments)
-constexpr uint16_t ASSERT_SEND_MAX_TIME_MS = 250;        // Max time the assert fail is allowed to wait to send header and message to HAL (will take up to 2x this since it sends 2 segments)
-constexpr uint16_t ASSERT_TAKE_MAX_TIME_MS = 500;        // Max time in ms to take the assert semaphore
+constexpr uint16_t ASSERT_BUFFER_MAX_SIZE = 160;  // Max size in bytes of assert buffers (assume x2 as we have two message segments)
+constexpr uint16_t ASSERT_SEND_MAX_TIME_MS = 250; // Max time the assert fail is allowed to wait to send header and message to HAL (will take up to 2x this since it sends 2 segments)
+constexpr uint16_t ASSERT_TAKE_MAX_TIME_MS = 500; // Max time in ms to take the assert semaphore
 
 /* System Functions ------------------------------------------------------------------*/
 //- Any system functions with an implementation here should be inline, and inline for a good reason (performance)
@@ -58,16 +59,21 @@ constexpr uint16_t ASSERT_TAKE_MAX_TIME_MS = 500;        // Max time in ms to ta
 // SOAR_PRINT macro, acts as an interface to the print function which sends a packet to the UART Task to print data
 #define SOAR_PRINT(str, ...) (cube_print(str, ##__VA_ARGS__))
 
+// SOAR_LOG macro, prints to UART AND logs variable-length text to external flash
+// Example Usage: SOAR_LOG("Sensor error at index %d", idx);
+#define SOAR_LOG(str, ...) (cube_log(str, ##__VA_ARGS__))
+
 /**
  * @brief Malloc inline function, wraps malloc for multi-platform support, asserts successful allocation
  * @param size Size of data to malloc in bytes
  * @return Returns the pointer to the allocated data
-*/
-inline uint8_t* cube_malloc(uint32_t size) {
+ */
+inline uint8_t *cube_malloc(uint32_t size)
+{
 #ifdef COMPUTER_ENVIRONMENT
-    uint8_t* ret = (uint8_t*)malloc(size);
+    uint8_t *ret = (uint8_t *)malloc(size);
 #else
-    uint8_t* ret = (uint8_t*)pvPortMalloc(size);
+    uint8_t *ret = (uint8_t *)pvPortMalloc(size);
 #endif
     SOAR_ASSERT(ret, "cube_malloc failed");
     return ret;
@@ -77,7 +83,8 @@ inline uint8_t* cube_malloc(uint32_t size) {
  * @brief Free inline function, wraps free for multi-platform support
  * @param ptr Pointer to the data to free
  */
-inline void cube_free(void* ptr) {
+inline void cube_free(void *ptr)
+{
 #ifdef COMPUTER_ENVIRONMENT
     free(ptr);
 #else
@@ -87,8 +94,7 @@ inline void cube_free(void* ptr) {
 
 /* Other ------------------------------------------------------------------*/
 // Override the new and delete operator to ensure heap4 is used for dynamic memory allocation
-inline void* operator new(size_t size) { return cube_malloc(size); }
-inline void operator delete(void* ptr) { cube_free(ptr); }
-
+inline void *operator new(size_t size) { return cube_malloc(size); }
+inline void operator delete(void *ptr) { cube_free(ptr); }
 
 #endif // CUBE_PLUSPLUS_CUBE_DEFINES_HPP_
